1c0ce54333feef343bc71653199d6fe34e67ca93
chamodya Wimansha <chamodyawimansha@gmail.com>
2023-03-13 18:31:00 +0530

    option printer added

---
+++ src/Cli.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Chmodw\ChatPHP;
+
+class Cli {
+
+    private array $options = [
+        'Chat',
+        'View History',
+        'Clear Database',
+        'Migrate Database'
+    ];
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * Prints available options to the terminal
+     *
+     * @return void
+     */
+    public function printOptions(): void
+    {
+        foreach ($this->options as $key => $option) {
+            $key = $key+1;
+            print_ln("\e[93m{$key}. \e[39m{$option}");
+        }
+
+        print_ln("________________________________");
+    }
+}
