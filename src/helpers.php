f88ac0389f9f73a6b03349b8f6947f23f21b6505
chamodya Wimansha <chamodyawimansha@gmail.com>
2023-03-13 15:36:45 +0530

    leading message print

--- src/helpers.php
+++ src/helpers.php
@@ -4,10 +4,15 @@ if (!function_exists('print_ln')) {
     /**
      * Print a single line to terminal
      *
-     * @param $string
+     * @param  string  $string
+     * @param  int  $leading_spaces
      * @return void
      */
-    function print_ln($string) {
-        echo $string . '\n';
+    function print_ln($string = "", $leading_spaces = 0) {
+        echo $string . "\n";
+
+        for ($i = 0;$i<$leading_spaces; $i++) {
+            echo "\n";
+        }
     }
 }
